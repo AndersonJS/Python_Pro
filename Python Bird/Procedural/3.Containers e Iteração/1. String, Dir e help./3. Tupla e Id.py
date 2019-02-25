093968a21bd5cb576caa269cd3c9b81126277760
AndersonJS <ajs.ti@hotmail.com>
2019-02-24 20:12:42 -0400

    Criando Tupla e fazer busca de id

--- Python Bird/Procedural/3.Containers e Iteração/1. String, Dir e help./3. Tupla e Id.py
+++ Python Bird/Procedural/3.Containers e Iteração/1. String, Dir e help./3. Tupla e Id.py
@@ -4,21 +4,21 @@ para identificar unicamente objetos durante a execução de um programa Python."
 
 tpl = (1, 2)
 print(type(tpl))
-#<class 'tuple'>
+# <class 'tuple'>
 
 print(tuple(range(6)))  # A tupla imprime dentro de parentes.
-#(0, 1, 2, 3, 4, 5)
+# (0, 1, 2, 3, 4, 5)
 
-tpl1 = (5,)  #Tupla com um unico número.tem de colocar a virgula no final pois se não colocar o python acha que é um int
+tpl1 = (5,)  # Tupla com um unico número.tem de colocar virgula no final pois se não colocar o python acha que é um int
 print(tpl1)
 print(type(tpl1))
 
 registro = ('Anderson', 30)
-nome, idade = registro  #Aqui é feito o desempacotamento por sua forte semantica.
+nome, idade = registro  # Aqui é feito o desempacotamento por sua forte semantica.
 print(nome)
 print(idade)
 
-"""Veremos como é criada uma nova tupla que agrega os dados de duas tuplas em apenas uma, mais não saberemos o seu id."""
+"""Veremos como é criada uma nova tupla que agrega os dados de duas tuplas em apenas uma, mais não saberemos o seu id"""
 registro_2 = ('Renzo', 35)
 print(registro + registro_2)
-print(id('_'))  #Quando perguntado o id do último valor impresso com '_' dentro do id ele retorna o id da soma das tuplas
+print(id('_'))  # Quando perguntado id do último valor impresso com '_' dentro do id ele retorna o id da soma das tuplas
