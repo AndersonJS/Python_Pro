f754c687ba360d3902291bb97ac09df106ac7793
AndersonJS <ajs.ti@hotmail.com>
2019-02-26 11:30:33 -0400

    Tipo de laço while com range.

---
+++ Python Bird/Procedural/3.Containers e Iteração/1. String, Dir e help./5.While.py
@@ -0,0 +1,9 @@
+"""Nesse tópico será abordado o primeiro tipo de laço, o While. Ele será utilizado em conjunto com range e o acesso a
+elementos para imprimir todos elementos de um container."""
+
+nome = 'Anderson'
+i = 0
+
+while i < len(nome):
+    print(nome[i])
+    i+= 1
