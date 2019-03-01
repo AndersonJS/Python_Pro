effbe374bc62029f531e21e121c5e0e8335a36d4
AndersonJS <ajs.ti@hotmail.com>
2019-03-01 18:31:43 -0400

    Vamos debuga com o break point.

---
+++ Python Bird/Procedural/4.Modulação/5.Debug.py
@@ -0,0 +1,12 @@
+""""Muitas vezes as pessoas começam a encher o código de print, pra identificar qual o resultado de algumas operações
+intermediarias ou chamada para debuga o código, uma das maneiras para se debuga o código também é utilizando a IDE,
+para utilizar o debug temos de fazer Break Points, para criar vá entre o número de contagem de linha e o começo da linha
+de código e click no numero da linha que você quer que o execução pare."""
+
+def soma(parcela, parcela_2):
+    return parcela + parcela_2
+
+print(soma(1, 2))
+
+"""As bolinhas vermelha são os break points, com o botão direito escolha a opção debug, para executar aperte o F7 e 
+executa cada break point que foi marcado."""
