780d72572a12641f82036218a6e8cf7153af4c71
AndersonJS <ajs.ti@hotmail.com>
2019-03-01 18:01:54 -0400

    Execução de módulo na IDE Pycharm, pelo terminal e no modo run.

---
+++ Python Bird/Procedural/4.Modulação/4.Módulo.py
@@ -0,0 +1,16 @@
+"""O módulo funciona como um script a ser executado pelo python."""
+
+def soma(parcela, parcela_2):
+    return parcela + parcela_2
+
+print(soma(1, 2))
+
+"""Esse módulo de script python pode ser executado de duas formas uma delas é pela linha de comando no terminal que tem
+dentro do próprio pycharm."""
+# Comandos
+# $ ls
+# Módulo.py
+# Python Módulo.py
+# 3
+
+"""A segunda maneira e clicar neste código com o botão direito na IDE pycharm e escolher a opção run."""
