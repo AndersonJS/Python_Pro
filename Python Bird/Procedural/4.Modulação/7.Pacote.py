b4d22db85c949a60e917b6950afe71ef530e1074
AndersonJS <ajs.ti@hotmail.com>
2019-03-02 17:50:44 -0400

    Módulos em Pacotes com o from, import e colocando o apelido a um trecho de script com o as

---
+++ Python Bird/Procedural/4.Modulação/7.Pacote.py
@@ -0,0 +1,9 @@
+"""Nesse tópico você vai aprender a organizar seu código em pacotes e também as diferentes maneiras de importar módulos.
+"""
+"""Os pacotes nada mais são do que pastas nas quais podemos colocar módulos para melhor organizar o nossos programas,
+então vamos supor que gostariamos de colocar modulos em pacote, poderiamos colocar os do repositorio ( 6.Exemplos Módulo
+e import), mais para não deixar confuso meu github não farei isso mais poderiamos fazer sem problema, vamos supor que eu
+movi o modulos matematema e o executar_matematica para o pacote matematica, então temos de dizer a IDE que eles foram 
+mudados de local e é isso que vamos fazer."""
+
+# Va ao Pacote (Exemplo Pacote)
---
+++ exemplo_pacote/executa_matematica.py
@@ -0,0 +1,10 @@
+from exemplo_pacote.mat import soma as s  # de exemplo_pacote.mat importe (ou busque) soma como s
+
+
+print(s(4, 5))
+
+"""O codido de importe esta referenciando o local onde as imformaçoes estão sendo buscada para a execução do script,
+podemos dizer que 'de (da pasta diretorio) exemplo_pacote (pegue o arquivo) mat e dentro deste arquivo quero o script 
+soma e que agora estou apelidando a soma de s"""
+
+#Basicamente é isso que a primeira linha diz.
