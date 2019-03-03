2c1fc1b3a89287033946221d979d0bec36ce490c
AndersonJS <ajs.ti@hotmail.com>
2019-03-03 01:51:01 -0400

    Uso da Docstring e comentário em um código.

---
+++ Python Bird/Procedural/4.Modulação/8.Docstring e Comentário.py
@@ -0,0 +1,7 @@
+"""Nesse tópico você vai aprender a documentar seu código com docstrings e comentários para aumentar e legibilidade. """
+
+# Comentário de uma linha
+
+"""Três aspas duplas é uma Docstring, comentário que tem mais de uma linha, é usando para documentar um programa.
+Varias vezes comentamos trechos do codigo para fazer algun teste e para comentar um trecho de codigo de forma rapida
+seleciona-se o trecho e aperta as teclas control + /"""
