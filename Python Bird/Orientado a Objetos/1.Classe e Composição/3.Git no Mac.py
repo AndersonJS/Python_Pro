8a4b570deed28c7db4f91b4d0c3f75e83f1b5740
AndersonJS <ajs.ti@hotmail.com>
2019-03-04 15:42:10 -0400

    Instalando o Homebrew e o Git no Mac.

---
+++ Python Bird/Orientado a Objetos/1.Classe e Composição/3.Git no Mac.py
@@ -0,0 +1,11 @@
+"""Para instalar o Git e não só ele mais também outra ferramenta que é um gerador de pacote que funciona de maneira
+semelhante ao gerenciador de pacote do Windows, estamos falando do 'Homebrew', você pode digite o nome Homebrew em seu
+navegador de internet que vai aparecer como primeiro retorno o site, entre no site e lá você vai encontrar um código
+que você deve copia-lo e executa-lo em seu terminal."""
+
+"""No terminal vocẽ vai colar o codigo do site e executar com a sua senha de admin.
+Depois de instalado você vai instalar o Git pelo Homebrew com o comando abaixo."""
+
+# brew install git
+
+"""Que ele vai instalar o Git em seu computador."""
