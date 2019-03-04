0b8c7934ef4826c2306c4b02162730664c8ec588
AndersonJS <ajs.ti@hotmail.com>
2019-03-04 14:58:44 -0400

    Instalando o Git no Windows.

---
+++ Python Bird/Orientado a Objetos/1.Classe e Composição/1.Git no Windows.py
@@ -0,0 +1,8 @@
+"""Para instalar o git no Windows, fça uma pesquisa no seu navegador de internet por 'git windows', ira aparecer
+'Git-Downloading Package', click na opção 'Download', vai abrir a pagina web e aparecer a opção para baixar, aperte em
+save ou salvar, depois que terminar o download, aperte em run ou executar."""
+
+"""no instalador aperte - next - next - next - next - next - next - next - next - next - Marque 'Launch Git Bash -
+Finish ou Finalizar."""
+
+"""Pronto o Git já esta instalado no seu sistema Windows."""
