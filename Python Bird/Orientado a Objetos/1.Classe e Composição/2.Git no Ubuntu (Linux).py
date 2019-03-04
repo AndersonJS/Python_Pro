8339ef24dfaa0f555b25c1945b827ecbdcc0b21d
AndersonJS <ajs.ti@hotmail.com>
2019-03-04 15:18:27 -0400

    Instalando o Git no Ubunto e derivados(Linux).

---
+++ Python Bird/Orientado a Objetos/1.Classe e Composição/2.Git no Ubuntu (Linux).py
@@ -0,0 +1,9 @@
+"""Em algumas distros Linux, o Git já vem instalado, então a primeira coisa que se pode fazer é abrir o terminal e
+digita por 'git', para conferir se ele já vem instalado, se não tiver instalado o terminal te ajuda com uma mensagem
+para resolver esse problema com um comando que instala o git em seu sistema."""
+
+
+# sudo apt-get install git
+
+"""Coloque a sua senha de admin, o 'y' para aceitar que baixe arquivos e pronto, vocẽ pode digitar git novamente e vai
+retorna o texto de help do Git."""
