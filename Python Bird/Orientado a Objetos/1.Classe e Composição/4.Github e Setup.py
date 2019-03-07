7b6a28731556349fcba5674791d3593e64d11ce9
AndersonJS <ajs.ti@hotmail.com>
2019-03-07 12:53:45 -0400

    Github, fazendo o primeiro fork de um projeto.

---
+++ Python Bird/Orientado a Objetos/1.Classe e Composição/4.Github e Setup.py
@@ -0,0 +1,25 @@
+"""Finalmente vamos fazer o setup do Python Birds e para fazer isso basta acessar:
+
+www.github.com/pythonprobr/pythonbirds
+
+Para fazer alteração no projeto, você precisa fazer uma cópia para o seu repositorio, e para fazer essa cópia se usa o
+fork.
+
+Agora vocẽ pode acessa o projeto em seu repositorio, mais você também deve ter acesso a esse projeto no seu computador.
+Em seu repositório entre no repositório que foi cópiado, procure pelo botão clone or donload.
+Não ensinarei o modo SSH, trabalharemos no modo HTTPS que sempre pede seu user e senha para se conectar ao seu
+repositório, você cópia o link de Https.
+
+Ai você vai abrir seu PyCharm, na pagina inicial tem a opção'Click out from version control', click nele e escolha Git
+e cole o HTTPS nele e click no botão clone.
+
+Se tiver um projeto já esta aberto no seu Pycharm, na barra de opção tem o 'VCS' é o mesmo 'Click out from versin
+control' é só abrir e cola o HTTPS nele e clickar no botão clone.
+
+Quando a cópia do projeto estive no seu computador o PyCharm vai pergunta se deseja abrir o projeto, click em sim, o
+PyCharm vai colocar todos os arquivos do projeto para você pode editar-los.
+
+No arquivo abra o 'placa_grafica_tkinter.py'
+
+Muitas vezes a placa grafica abre atrás a interfase do PyCharm, na barra de programas abertos, veja que já esta aberta
+a interfase do tkinter"""
