289096de32b8293b60ea4eccc4130c726ae34427
AndersonJS <ajs.ti@hotmail.com>
2019-03-07 13:51:55 -0400

    Atalhos de commit e push no PyCharm

---
+++ Python Bird/Orientado a Objetos/1.Classe e Composição/6.Commit e Push.py
@@ -0,0 +1,6 @@
+"""Para enviar suas alterações e criações ao gihub, procure na barra superior do PyCharm por Git e uma seta azul que
+atualizar seus repositório e ao lado tem outro simbolo que parece uma marcação de certo verde, este certo verde é o
+commit e o atalho para ele é ctrl + k.
+
+Já o push uso de duas forma, uma que é pelo terminal que esta dentro do PyCharm, abro ele e digito git push e aperto o
+enter e outra forma é pelo atalho crtl + SHIFT + K."""
