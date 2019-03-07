b7a49b6d9782eb2d2ff5e884739852e74597d911
AndersonJS <ajs.ti@hotmail.com>
2019-03-07 13:28:03 -0400

    Criando uma classe

---
+++ Python Bird/Orientado a Objetos/1.Classe e Composição/5.Classe.py
@@ -0,0 +1,18 @@
+# Todos os pacotes e conteúdos criados seram no repositório Python Pro que fizemos o fork.
+"""Para iniciar o estudo de Orientação a Objetos vou cria um pacote chamado OO, que é de Orientação a Objetos, vai
+aparecer uma opção perguntando se você que adicionar o arquivo que esta criando no controlador de versão, aperte em
+'remember, dont' ask again' é para lembrar esta escolha e depois click em yes."""
+
+# No pacote crie um módulo chamado pessoa.
+
+"""No módulo crie uma classe, lembre que classe começa com letra maiuscula e se tiver mais de uma palavras as duas devem
+começa em maiusculas, Ex: class ExemploPessoa:
+    class Pessoa:
+        pass
+        
+Para importar uma classe é semelhante a uma função:
+
+>>> from OO.pessoa import pessoa
+>>> p = Pessoa()
+>>> type(p)
+<class 'OO.pessoa.Pessoa"""
